a4859b979ccc8128490c7ffe362051a19a54a575
Brett <bretttucker@gmail.com>
2017-02-06 19:54:35 -0700

    Customize service response
    We will customer the response returned from each service instance with
    an environment variable that will be provided in the docker-compose.yml

--- src/main/java/com/abnormallydriven/dockercompose/Application.java
+++ src/main/java/com/abnormallydriven/dockercompose/Application.java
@@ -4,6 +4,7 @@ import static spark.Spark.*;
 
 public class Application {
     public static void main(String[] args) {
-        get("/hello", (request, response) -> "Hello World!");
+        String serviceName = System.getenv("service_name");
+        get("/hello", (request, response) -> "Hello from " + serviceName);
     }
 }
